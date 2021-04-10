832d4a69eb85adad4e7d58d0174c1d684e0a440f
Shisunlel <worlddominator007@gmail.com>
2021-04-10 10:11:39 +0700

    remove unnecessary table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -28,7 +29,7 @@ class UserFactory extends Factory
             'username' => $this->faker->userName,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('123'),
             'id_card' => $this->faker->imageUrl(500, 500, 'users', true),
             'phone_number' => $this->faker->unique()->randomNumber(9, true),
             'housenumber' => $this->faker->randomNumber(5, false),
