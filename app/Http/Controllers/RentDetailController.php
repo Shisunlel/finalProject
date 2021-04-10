d0deb26e82fc59a8bcf4b11b585b6bb5b6866499
Shisunlel <worlddominator007@gmail.com>
2021-04-10 20:23:12 +0700

    add middleware for history route

--- app/Http/Controllers/RentDetailController.php
+++ app/Http/Controllers/RentDetailController.php
@@ -6,9 +6,14 @@ use App\Models\DetailRent;
 
 class RentDetailController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function history()
     {
-        $detail = DetailRent::join('rents', 'rent_id', '=', 'id')->join('rooms', 'room_id', 'rooms.id')->where('rents.user_id', auth()->id())->select('title', 'description', 'duration', 'cost', 'total', 'rents.created_at')->get();
+        $detail = DetailRent::join('rents', 'rent_id', '=', 'id')->join('rooms', 'room_id', 'rooms.id')->where('rents.user_id', auth()->id())->select('title', 'description', 'duration', 'cost', 'total', 'rents.created_at')->orderBy('rent_id')->get();
         return view('auth.history')->with('detail', $detail);
     }
 }
